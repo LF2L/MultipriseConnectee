a6da21dd3db0513bd4c0e48910626ce6ee032696
gabriel-alex <trist4n@Trist4n.local>
2017-06-28 10:16:10 +0200

    update

---
+++ MultiPriseRTC-JSON/MultiPriseRTC-JSON.ino
@@ -0,0 +1,216 @@
+/*
+    This sketch demonstrates how to set up a simple HTTP-like server.
+    The server will set a GPIO pin depending on the request
+      http://server_ip/gpio/0 will set the GPIO2 low,
+      http://server_ip/gpio/1 will set the GPIO2 high
+    server_ip is the IP address of the ESP8266 module, will be
+    printed to Serial when the module is connected.
+*/
+
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include "ds3231.h"
+#include <ArduinoJson.h>
+
+struct IoTobject{
+  IPAddress ip;
+  char* id;
+  char* device_type;
+  
+  void printconsole(){
+     Serial.println("info into IoTObject ");
+     Serial.print(" IP: ");
+     Serial.println(ip);
+     Serial.print(" id: ");
+     Serial.println(id);
+     Serial.print(" device: ");
+     Serial.println(device_type);
+  }
+};
+IoTobject monit;
+
+const unsigned long BAUD_RATE = 9600;
+const int BUFF_MAX=128;
+
+const char* ssid = "IoE Server";
+const char* password = "Jaedeo13ecmden13";
+
+// Create an instance of the server
+// specify the port to listen on as an argument
+WiFiServer server(80);
+
+boolean plg[6] ;
+int plgPin[] ={9,10,14,12,13,15};
+
+uint8_t timet[8];
+
+void setup() {
+  initSerial();
+  Wire.begin();
+  DS3231_init(DS3231_INTCN);
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  delay(100);
+
+
+  // prepare GPIO
+  pinMode(plgPin[1], OUTPUT);
+  pinMode(plgPin[2], OUTPUT);
+  pinMode(plgPin[3], OUTPUT);
+  pinMode(plgPin[4], OUTPUT);
+  pinMode(plgPin[5], OUTPUT);
+  pinMode(plgPin[6], OUTPUT);
+  digitalWrite(plgPin[1], 0);
+  digitalWrite(plgPin[2], 0);
+  digitalWrite(plgPin[3], 0);
+  digitalWrite(plgPin[4], 0);
+  digitalWrite(plgPin[5], 0);
+  digitalWrite(plgPin[6], 0);
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+
+
+}
+
+void loop() {
+  PowerStrip ps;
+  //for time management 
+  char buff[BUFF_MAX];
+  struct ts t;
+  
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+   DS3231_get(&t);
+   snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d %ld", t.year,
+             t.mon, t.mday, t.hour, t.min, t.sec);
+  // Wait until the client sends some data
+  Serial.println(buff);
+  Serial.println("- new client");
+  while (!client.available()) {
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+
+  // Match the request
+
+   if (req.indexOf("/plug/1") != -1)
+    plg[1] = (plg[1] == 0) ? 1 : 0;
+  else if (req.indexOf("/plug/2") != -1)
+    plg[2] = (plg[2] == 0) ? 1 : 0;
+  else if (req.indexOf("/plug/3") != -1)
+    plg[3] = (plg[3] == 0) ? 1 : 0;
+  else if (req.indexOf("/plug/4") != -1)
+    plg[4] = (plg[4] == 0) ? 1 : 0;
+  else if (req.indexOf("/plug/5") != -1)
+    plg[5] = (plg[5] == 0) ? 1 : 0;
+  else if (req.indexOf("/plug/6") != -1)
+    plg[6] = (plg[6] == 0) ? 1 : 0;
+  else {
+    Serial.println("invalid request");
+    client.stop();
+    return;
+  }
+ 
+  StaticJsonBuffer<500> jsonBuffer;
+  JsonObject& json = prepareResponse(jsonBuffer);
+  writeResponse(client, json);
+  
+  digitalWrite(plgPin[1], plg[1]);
+  digitalWrite(plgPin[2], plg[2]);
+  digitalWrite(plgPin[3], plg[3]);
+  digitalWrite(plgPin[4], plg[4]);
+  digitalWrite(plgPin[5], plg[5]);
+  digitalWrite(plgPin[6], plg[6]);
+  
+  client.flush();
+
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected
+  // when the function returns and 'client' object is detroyed
+}
+void initSerial() {
+  Serial.begin(BAUD_RATE);
+  while (!Serial) {
+    ;  // wait for serial port to initialize
+  }
+  Serial.println("Serial ready");
+}
+
+JsonObject& prepareResponse(JsonBuffer& jsonBuffer) {
+  char buff[BUFF_MAX];
+  struct ts t;
+   DS3231_get(&t);
+   snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d %ld", t.year,
+             t.mon, t.mday, t.hour, t.min, t.sec);
+             
+  JsonObject& root = jsonBuffer.createObject();
+  root["device_type"] = "PowerStrip";
+  root["ip_address"] = WiFi.localIP();
+  root["id"] = 00000;
+  root["nb_socket"] = ps.NbSocket;
+  root["time"] = t.hour+":"+t.min;
+
+  JsonArray& socket = root.createNestedArray("socket");
+  for (int soc = 0; soc < ps.NbSocket; soc++) {
+    socket.add("state",plg[soc]);
+  }
+
+  return root;
+}
+void writeResponse(WiFiClient& client, JsonObject& json) {
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: application/json");
+  client.println("Connection: close");
+  client.println();
+
+  json.prettyPrintTo(client);
+}
+
+bool sendIotInfo(){
+  http.begin("http://192.168.42.1:5000/objectport/");
+  http.addHeader("Content-Type", "application/json");
+  String s= "{\"ip_adress\": ";
+  s+=monit.ip;
+  s+=", \"id\": ";
+  s+=monit.id;
+  s+=",\"device_type\": ";
+  s+=monit.device_type;
+  s+="}";
+  int postCode = http.POST(s);
+  // int postCode = http.POST("{\"ip_adress\": \"192.168.42.17\", \"id\": \"00001\",\"device_type\": \"monitor\"}");
+  http.writeToStream(&Serial);
+  if (postCode == HTTP_CODE_OK){
+    return true; 
+  }else{
+    return false;
+  }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# Multiprise connectée
+
+## Description
+La multiprise connectée permet de contrôler individuellement chacune des prises à distance mais également de programmer le déclenchement selon des plages horaires. 
+  
+## Fonction
+* Alimenter chacune des prises en fonction de la demande de l’utilisateur 
+* Déclencher les prises en fonction des plages horaires programmées 
+* Couper l’alimentation électrique lors de demande délestage 
+
+## Composants
+* Node MCU ESP8266
+* RTC 
+* Relai 5v 6 canaux
+* Convertisseur 220V AC- 5V DC
+* Câbles 230V
+* Multiprise
+
+## Boitiers
+* Acrylique 3mm
+
+## Echanges d'informations
+La multiprise va se connecter à un serveur qui centralise les informations concernant les objets connectés. Il possède entre autre les informations des puces RFID avec les autorisations et les statistiques de consommation (durée et puissance).  A l’instar des formulaires de connexion sur le site web, les données d’identification doivent certainement faire l’objet d’un « hashage ».
+
+### Informations récupérées
+Pour fonctionner la prise doit uniquement recevoir une autorisation d’alimenter la prise. 
+
+### Informations envoyées
+Inversement la seule variable de fonctionnement de la prise connectée est l’UID de la puce RFID.
+
+### Fonctionnement
+Lors de la détection de la présence d’une puce RFID, la prise connectée envoie l’information de la carte (préférablement sécurisée) au serveur REST qui centralise les informations dont celles des identifiants UID des puces, des autorisations et des statistiques de consommation. En fonction, des règles de fonctionnement appliquées par le serveur, il renvoie une autorisation d’activer ou non la prise électrique. 
+La prise étant connectée au serveur, il est possible de désactiver la prise lors d’une demande de délestage. 
+
+## To do
+* Réaliser les fonctions REST du serveur pour la commande de l’objet.
+* Script d’envoie du statut de l’objet et de réception des commande.
+* Fonction de programmation temporelle et déclenchement des prises.
+
+
